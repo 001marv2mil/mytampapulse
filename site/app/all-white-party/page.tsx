70c99f303c3520887ea78b08ccb2c299a67921c0
001marv2mil <001marv2mil@users.noreply.github.com>
2026-07-02 17:58:24 -0400

    Play button: use muPO1c6pxXg starting at 0:44

--- site/app/all-white-party/page.tsx
+++ site/app/all-white-party/page.tsx
@@ -400,7 +400,7 @@ function AllWhitePartyCheckout() {
       {playing && (
         <div aria-hidden="true" className="fixed top-0 -left-[9999px] w-80 h-44 opacity-0 pointer-events-none">
           <iframe
-            src="https://www.youtube.com/embed/LDY_XyxBu8A?autoplay=1&rel=0"
+            src="https://www.youtube.com/embed/muPO1c6pxXg?start=44&autoplay=1&rel=0"
             title="SZA - Snooze (audio)"
             allow="autoplay; encrypted-media"
             className="w-full h-full"
